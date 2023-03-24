dd03fa5ae265dfb6c1d23b66d42d8422a325cef4
Kamal3113 <123230758+Kamal3113@users.noreply.github.com>
2023-03-24 15:57:08 +0530

    yes

---
+++ lib/model/product_model.dart
@@ -0,0 +1,12 @@
+class Product {
+  final int id;
+  final String productName;
+  final String productImage;
+  final String productDescription;
+
+  Product(
+      {required this.id,
+      required this.productName,
+      required this.productDescription,
+      required this.productImage});
+}
