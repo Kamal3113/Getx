8e39fde5fc117354fb440bc11b5491df66632915
Kamal3113 <123230758+Kamal3113@users.noreply.github.com>
2023-03-24 16:11:45 +0530

    new lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:getapp/view/audio_player.dart';
+
+import 'package:getxstatemanagement/views/product_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: dusicPage()
-        //const MyHomePage(title: 'Flutter Demo Home Page'),
-        );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  double pw = Get.size.width;
-  double ph = Get.size.height;
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      debugShowCheckedModeBanner: false,
+      title: 'StateManagement with getx',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              height: ph * (0.3),
-              width: pw * (0.5),
-              color: Colors.red,
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: ProductScreen(),
     );
   }
 }
